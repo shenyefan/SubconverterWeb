1e92bbe7687d44aa172e02155e5f53b211ac550b
shenyefan <shenyefan0793@163.com>
2024-01-29 18:54:32 +0800

    修改导航栏

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -2,13 +2,13 @@
   <div class="navbar">
     <router-link to="/" class="logo">
       <img src="@/assets/logo.png" alt="订阅转换" />
-      <span class="brand-name">Subconverter Web</span>
+      <span class="brand-name">Sub Web</span>
     </router-link>
 
     <div class="nav-buttons">
-      <a href="https://shenyefan.cn/" class="nav-link">首页</a>
-      <router-link to="/" class="nav-link">订阅转换</router-link>
-      <a href="https://suki.icu/" class="nav-link">短链生成</a>
+      <a href="https://shenyefan.cn/" class="nav-link">Blog</a>
+      <!--<router-link to="/" class="nav-link">订阅转换</router-link>-->
+      <a href="https://suki.icu/" class="nav-link">ShortURL</a>
     </div>
 
   </div>
