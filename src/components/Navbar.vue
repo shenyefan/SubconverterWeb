a7121999defa989dc3e086c5f3549f9ddf797685
shenyefan <shenyefan0793@163.com>
2024-08-06 00:43:48 +0800

    0.2.0

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -2,7 +2,7 @@
   <div class="navbar">
     <router-link to="/" class="logo">
       <img src="@/assets/logo.png" alt="订阅转换" />
-      <span class="brand-name">Sub Web</span>
+      <span class="brand-name">SubConverter Web</span>
     </router-link>
   </div>
 </template>
